18cad8f447b3f4e3e01225d3c635f9cbeae75b4d
Arup-SMB <arupmoulick1@gmail.com>
2024-05-30 21:22:15 +0530

    New File

---
+++ carousel.js
@@ -0,0 +1,5 @@
+$(".banner").owlCarousel({
+    items: 1,
+    autoplay:true,
+    loop: true,
+})
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./Project-For-GitHub/*.html"],
+  content: ["./*.html"],
   theme: {
     extend: {},
   },
