2bb49234430ffd4e87db2bf22fc9bbb48876c79c
Arup-SMB <arupmoulick1@gmail.com>
2024-06-21 18:39:50 +0530

    Complete

--- carousel.js
+++ carousel.js
@@ -2,18 +2,32 @@ $(".banner").owlCarousel({
     items: 1,
     autoplay:true,
     loop: true,
+    dots: false,
 })
 $(".product").owlCarousel({
     items: 3,
     autoplay:true,
     loop: true,
     margin: 20,
+    dots: false,
 })
+
+var slider = $('.owl-carousel');
+
+$(".carousel-prev").click(function() {
+    slider.trigger("prev.owl.carousel")
+});
+
+$(".carousel-next").click(function() {
+    slider.trigger("next.owl.carousel")
+});
+
 $(".testimonials").owlCarousel({
     items: 1,
     autoplay:true,
     loop: true,
     margin: 20,
+    dots: true,
 })
 // const menuToggle = document.getElementById('menu-toggle');
 // const menu = document.getElementById('menu');
